8a7f8aa63b115d7907914b8bbe65a824bd76a16c
Dafydd <me@daf.lol>
2024-07-23 23:40:26 +0100

    Fix ID config

--- txt-to-aiir.py
+++ txt-to-aiir.py
@@ -10,7 +10,7 @@ while True:
                                 if '-' in line:
                                         artist, song = line.strip().split(' - ')
                                         data = {'type': "song", 'artist': artist, 'title': song}
-                                        response = requests.post(f"https://api.aiir.net/v1/services/{aiir_password}/now-playing?artist={quote(artist)}&title={quote(song)}&type=song&password={quote(aiir_password)}", data=data)
+                                        response = requests.post(f"https://api.aiir.net/v1/services/{aiir_id}/now-playing?artist={quote(artist)}&title={quote(song)}&type=song&password={quote(aiir_password)}", data=data)
         except:
                 print('File mid update')
         sleep(15)
